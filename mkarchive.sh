1154879d0c65efff549dee696d07e86d2b3898b3
Bill Pemberton <wfp5p@worldbroken.com>
2015-04-16 10:19:03 -0400

    Update mkarchive to add spec file to tar

--- mkarchive.sh
+++ mkarchive.sh
@@ -4,7 +4,11 @@
 
 ELM_VERSION=$(sed -e 's/^ELM_VERSION = //' < ELM-VERSION-FILE)
 
-git archive --format=tar --prefix=elm-$ELM_VERSION/ HEAD |gzip >elm-$ELM_VERSION.tar.gz
+git archive --format=tar --prefix=elm-$ELM_VERSION/ HEAD -o elm-$ELM_VERSION.tar
 
 sed "s|ELM-VERSION-STRING|$ELM_VERSION|" elm.spec.in >elm.spec
+
+tar -rf elm-$ELM_VERSION.tar --xform "s,^,elm-$ELM_VERSION/," elm.spec
+
+pigz elm-$ELM_VERSION.tar
 
