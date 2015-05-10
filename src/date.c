13f863f007705ff838bac4ccf43c3d658a0c1409
Bill Pemberton <wfp5p@worldbroken.com>
2015-05-10 15:47:11 -0400

    date.c: remove days_ahead() and month_number()
    These functions are not used

--- src/date.c
+++ src/date.c
@@ -53,64 +53,6 @@ extern time_t	  time();
 static int  days_in_month[] = { 31,    28,    31,    30,    31,     30,
 		  31,     31,    30,   31,    30,     31,  -1};
 
-static void days_ahead(int days, char *buffer)
-{
-	/** return in buffer the date (Day, Mon Day, Year) of the date
-	    'days' days after today.
-	**/
-
-	struct tm *the_time;		/* Time structure, see CTIME(3C) */
-	time_t	   junk;		/* time in seconds....		 */
-
-	junk = time((time_t *) 0);	/* this must be here for it to work! */
-	the_time = localtime(&junk);
-
-	/* increment the day of the week */
-
-	the_time->tm_wday = (the_time->tm_wday + days) % 7;
-
-	/* the day of the month... */
-	the_time->tm_mday += days;
-
-        while (the_time->tm_mday > days_in_month[the_time->tm_mon]) {
-          if (the_time->tm_mon == FEB && leapyear(the_time->tm_year+1900)) {
-            if (the_time->tm_mday > DAYS_IN_LEAP_FEB) {
-              the_time->tm_mday -= DAYS_IN_LEAP_FEB;
-              the_time->tm_mon += 1;
-            }
-            else
-              break;            /* Is Feb 29, so leave */
-          }
-          else {
-            the_time->tm_mday -= days_in_month[the_time->tm_mon];
-            the_time->tm_mon += 1;
-          }
-
-          /* check the month of the year */
-          if (the_time->tm_mon > MONTHS_IN_YEAR) {
-            the_time->tm_mon -= (MONTHS_IN_YEAR + 1);
-            the_time->tm_year += 1;
-          }
-        }
-
-        /* now, finally, build the actual date string */
-        the_time->tm_mday %= 32,
-	strftime(buffer, SLEN, "%a, %d %B %y", the_time);
-}
-
-static int month_number(char *name)
-{
-	int retval;
-	if (cvt_monthname_to_monthnum(name, &retval) < 0) {
-		/* conversion failed ... pick January */
-		retval = 0;
-	} else {
-		/* conversion routine uses Jan=1, we want Jan=0 */
-		--retval;
-	}
-	return retval;
-}
-
 char *elm_date_str(char *buf, struct header_rec *entry)
 {
 	time_t secs = (entry->time_sent + entry->tz_offset);
